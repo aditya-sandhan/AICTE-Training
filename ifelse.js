626adc476f5b073daa8726ec0fa3ca04b98ae776
Aditya <adityasandhan.ai@gmail.com>
2026-01-05 15:59:48 +0530

    Add initial if-else structure for weight classification

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+let weight =18.5;
+
+if (weight < 18.5) {
+    console.log("Invalid weight");
+}
+else if (weight >= 18.5 && weight < 24.9) {
+    console.log("Normal weight");
+}
+else if (weight >= 25 && weight < 29.9) {
+    console.log("Overweight");
+}
+else {
+    console.log("Obese");
+}
